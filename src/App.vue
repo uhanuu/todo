38c646e0542d9a2fb1f4121e8615f2be1baf24b0
uhanuu <hwryu990927@gmail.com>
2024-07-06 16:16:12 +0900

    feat: input box

--- src/App.vue
+++ src/App.vue
@@ -14,4 +14,20 @@ import TodoInput from './components/TodoInput.vue';
 import TodoList from './components/TodoList.vue';
 </script>
 
-<style lang="scss" scoped></style>
+<style>
+body {
+    text-align: center;
+    background-color: #f6f6f6;
+}
+
+input {
+    border-style: groove;
+    width: 200px;
+}
+button {
+    border-style: groove;
+}
+.shadow {
+    box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03);
+}
+</style>
--- src/components/TodoInput.vue
+++ src/components/TodoInput.vue
@@ -1,7 +1,48 @@
 <template>
-    <div>input</div>
+    <div class="inputBox shadow">
+        <input type="text" v-model="newTodoItem" @keyup.enter="addTodo" />
+        <span class="addContainer" @click="addTodo">
+            <i class="addBtn">추가</i>
+        </span>
+    </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
 
-<style scoped></style>
+const newTodoItem = ref('');
+const addTodo = () => {
+    localStorage.setItem(newTodoItem.value, newTodoItem.value);
+    clearInput();
+};
+const clearInput = () => {
+    newTodoItem.value = '';
+};
+</script>
+
+<style scoped>
+input:focus {
+    outline: none;
+}
+.inputBox {
+    background: white;
+    height: 50px;
+    line-height: 50px;
+    border-radius: 5px;
+}
+.inputBox input {
+    border-style: none;
+    font-size: 0.9rem;
+}
+.addContainer {
+    float: right;
+    background: linear-gradient(to right, #6478fb, #8763fb);
+    display: block;
+    width: 3rem;
+    border-radius: 0 5px 5px 0;
+}
+.addBtn {
+    color: white;
+    vertical-align: middle;
+}
+</style>
